8b12842adaf6eb7d5632779c331aca43a36e922a
Mikhail Rodovolskiy <38501368+freed38@users.noreply.github.com>
2023-01-15 22:49:27 +0100

    Delete Django Project directory

--- Django Project/mysite/goin_backend/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Location,GenreTag,Band,Event
-
-admin.site.register(Location)
-admin.site.register(GenreTag)
-admin.site.register(Band)
-admin.site.register(Event)
--- Django Project/mysite/goin_backend/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Goin_backendConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "goin_backend"
